68e3aba0feee9a22c1995437fb9817903686a151
Oksihris <112577436+Oksihris@users.noreply.github.com>
2023-06-08 18:56:59 +0300

    Token expired and chat m

--- Server/Controllers/UsersController.cs
+++ Server/Controllers/UsersController.cs
@@ -24,6 +24,37 @@ namespace BlazorChat.Server.Controllers
             await _chatContext.Users.AsNoTracking().Where(u => u.Id != UserId)
             .Select(u => new UserDto(u.Id, u.Name, false))
             .ToListAsync();
+
+        [HttpGet("chats")]
+        public async Task<IEnumerable<UserDto>> GetUserChats(CancellationToken cancellationToken)
+        {
+            IEnumerable<UserDto> chatUsers = new List<UserDto>();
+            var uniqueUsers = await _chatContext.Messages
+                       .AsNoTracking()
+                       .Where(m => m.FromId == UserId || m.ToId == UserId)
+                       .Select(m => new { From = m.FromId, To = m.ToId })
+                       .Distinct()
+                       .ToListAsync(cancellationToken);
+
+            var uniqueUserIds = new HashSet<int>();
+            uniqueUsers.ForEach(u =>
+            {
+                if (u.From != UserId)
+                    uniqueUserIds.Add(u.From);
+                if (u.To != UserId)
+                    uniqueUserIds.Add(u.To);
+            });
+            if (uniqueUserIds.Count > 0)
+            {
+                chatUsers = await _chatContext.Users
+                                    .AsNoTracking()
+                                    .Where(u => uniqueUserIds.Contains(u.Id))
+                                    .Select(u => new UserDto(u.Id, u.Name, false))
+                                    .ToListAsync(cancellationToken);
+            }
+            return chatUsers;
+        }
+
     }
 }
        
